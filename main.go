af3d06fc1389974cdccd7555081a56b8148236da
Roman Müntener <me@mroman.ch>
2017-06-13 14:02:23 +0200

    + invoke EnableHeuristics()

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import "os"
 import "fmt"
 import "github.com/google/gopacket/pcap"
 import "github.com/google/gopacket"
+import "github.com/google/gopacket/layers"
 
 func main() {
+	layers.EnableHeuristics()
+
 	if handle, err := pcap.OpenOffline(os.Args[1]); err != nil {
 		panic(err)
 	} else {
